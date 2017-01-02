2ce8d9f7da0d1aad18d120b23de9c6a2e30e26a4
yzc19941020 <304614345@qq.com>
2017-01-03 06:52:54 +0800

    搜索模块
    iiiiiiii

---
+++ routes/search.js
@@ -0,0 +1,33 @@
+/**
+ * Created by yzc on 2017/1/2.
+ */
+var express = require('express');
+var router = express.Router();
+var usr = require('../connectDB.js');
+var webApi = require('../WebAPI.js');
+/* GET home page. */
+router.get('/search', function(req, res) {
+
+    client = usr.connect();
+    result = null;
+    var search = "安静";
+    usr.querySinger(client,search,function(result){
+        if (result[0] == undefined) {
+            webApi.insertSong(search);
+            usr.querySinger(client,search,function(result){
+                if (result[0] == undefined) {
+                    res.send("找不到结果！");
+                }
+                else
+                    res.send(JSON.stringify(result));
+            });
+        }
+        else
+            res.send(JSON.stringify(result));
+    });
+    //res.render('index', { title: 'Express' });
+
+
+});
+
+module.exports = router;
